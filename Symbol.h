2cf3d2a2217f328bbd4e4a050e06887f4ce00ff6
shak1522 <shakedmann1@gmail.com>
2022-07-23 17:12:26 +0300

    Sym

--- Symbol.h
+++ Symbol.h
@@ -2,9 +2,6 @@
 
 #ifndef SYMBOL_H /*!!!!!!!!!!!!!*/
 #define SYMBOL_H /*!!!!!!!!!!!!!*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <linkedList.h>
 #include <symbolTable.h>
 typedef bool *IsEqual(void *data, void *parameter);
 
@@ -34,8 +31,8 @@ void delete_symbol(Symbol *sym, SymbolTable symTable);
  */
 
 
-bool add_value(Symbol *symbol, void *data);
+bool add_Command(Symbol *symbol, void *data);
 
 /**Searches if the data already exists in the symbol*/
-void *search_symbol(SymbolTable *symbol_table, Symbol *symbol, char *name, IsEqual is_equal);
+void *search_symbol(Symbol *symbol, SymbolTable *symbol_table, char *name, IsEqual is_equal);
 #endif
