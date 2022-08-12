f6adbeeeb6777e263c09d8dd8bf17fab9403bede
shak1522 <shakedmann1@gmail.com>
2022-08-12 23:16:33 +0300

    er

--- error_handling.c
+++ error_handling.c
@@ -17,7 +17,7 @@ void alertFileError(char* desc)
 *A global alert that that prints an error message in regards to an error during a pass (compilation error)
 *@desc: The description of the error
 */
-void printError(char *desc )
+void alertError(char *desc )
 {
 	fprintf(stderr, "[%s]->[Please revise line - %d] %s %s]\n", filename, linenumber, "[Error-", desc);
 }
--- error_handling.h
+++ error_handling.h
@@ -3,7 +3,12 @@
 
 #include "global.h"
 
+/* File errors */
+#define FILE_NAME_MEM_ALLOCATION_FAILURE "Could not allocate memory for the file name you provided"
 
+#define ERROR_CREATE_FILE	"Could not create file"
+#define MISSING_INPUT_FILE "missing input files "
+#define FILENAME_TOO_LONG "filename is too long"
 
 /*File Errors*/
 #define ER_OPEN_FILE "could not open the file.\n"
@@ -13,9 +18,9 @@
 /*Label Errors*/
 #define ER_LABEL_NOT_FIRST_ALPHA_NUMERIC "Label not starting with alphanumeric\n"
 #define ER_LABEL_TOO_LONG "The label is too long, max length allowed is 30 chars.\n"
-#define ER_LABEL_NAME_ILLEGAL"Label is a keyword or a register.\n"
+#define ER_LABEL_NAME_ILLEGAL "Label is a keyword or a register.\n"
 #define ER_LABEL_SPACE "There is a space before the colon in label declaration.\n"
-#define ER_LABEL_ALPHA"Label is not entirely alpha numeric\n"
+#define ER_LABEL_ALPHA "Label is not entirely alpha numeric\n"
 #define LABEL_ALREADY_EXISTS "Label already exists.\n"
 
 /*Instruction Errors*/
