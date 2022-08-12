6d07cd120dbc711a8fd4a7de63779b6b63fa5369
shak1522 <shakedmann1@gmail.com>
2022-08-12 21:28:08 +0300

    error

--- error_handling.c
+++ error_handling.c
@@ -1,1 +1,14 @@
 #include "error_handling.h"
+
+
+
+
+/**Description: Prints an error while opening or creating a file
+ *Parameters:
+ *@desc: The description of the error
+ *@fileName: the name of the file
+ */
+void alertFileError(char* desc, char* fileName)
+{
+	fprintf(stderr, "[%s] : %s %s\n", fileName, "[Error] -> ", desc);
+}
--- error_handling.h
+++ error_handling.h
@@ -1,10 +1,22 @@
 #ifndef ERRORHANDLING_H
 #define ERRORHANDLING_H
+
+
+/*File Errors*/
+#define OPEN_FILE_ERROR "could not open the file.\n"
 #define FILENOTFOUND "The requested file %s not found.\n"
 #define LABEL_ALREADY_EXIST "The label %s previously declared.\n"
 #define INSTRUCTION_NOT_EXIST "No such instraction %s.\n"
 #define NO_FILE_SPECIFIED "No file specified for the compiler.\n"
 #define LABEL_ALREADY_EXISTS "Label already exists.\n"
+
+
+/**Description: Prints an error while opening or creating a file
+ *Parameters:
+ *@desc: error description
+ *@fileName: the file name
+ */
+void alertFileError(char *desc, char *filename);
 
 void add_error(char *err);
 #endif
